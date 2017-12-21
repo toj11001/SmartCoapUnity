ffc6db1f820459e6da84bb347c743c4e3a175970
Tobias <tobias.joschko@gmx.de>
2017-12-21 12:42:44 +0100

    BarChart Until Part 3

---
+++ Assets/Scripts/BarChart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+public class BarChart : MonoBehaviour {
+
+	public Bar barPrefab;
+	public int[] InputValues;
+	List<Bar> bars = new List<Bar>(); //List to store the Bars
+
+	float ChartHeight;
+
+	void Start () {
+		ChartHeight = Screen.height + GetComponent<RectTransform> ().sizeDelta.y;
+
+		//Display the graph
+		DisplayGraph(InputValues);
+	
+	}
+
+	void DisplayGraph(int[] vals){
+		int maxVal = vals.Max ();
+
+		for (int i = 0; i < vals.Length; i++) {
+			Bar newBar = Instantiate (barPrefab) as Bar; 
+			newBar.transform.SetParent (transform);
+			RectTransform rt = newBar.bar.GetComponent<RectTransform> ();
+			float normalizeVal = (float)vals [i] / (float)maxVal;
+			rt.sizeDelta = new Vector2 (rt.sizeDelta.x, ChartHeight * normalizeVal);
+		}
+	}
+}
