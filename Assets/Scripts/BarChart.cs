9bfda4404507071878bb81eb6ac360721a73857e
Tobias <tobias.joschko@gmx.de>
2017-12-21 14:28:07 +0100

    added color input array. Setting the colors doesn´t work yet tho...

--- Assets/Scripts/BarChart.cs
+++ Assets/Scripts/BarChart.cs
@@ -10,6 +10,7 @@ public class BarChart : MonoBehaviour {
 	public int[] InputValues;
 	List<Bar> bars = new List<Bar>(); //List to store the Bars
 	public string[] labels; //Input labels
+	public Color[] colors; //Input Colors
 
 	float ChartHeight;
 
@@ -27,16 +28,26 @@ public class BarChart : MonoBehaviour {
 		for (int i = 0; i < vals.Length; i++) {
 			Bar newBar = Instantiate (barPrefab) as Bar; 
 			newBar.transform.SetParent (transform);
+			//size bar
 			RectTransform rt = newBar.bar.GetComponent<RectTransform> ();
 			float normalizeVal = (float)vals [i] / (float)maxVal;
 			rt.sizeDelta = new Vector2 (rt.sizeDelta.x, ChartHeight * normalizeVal);
-
+			//set bar color
+//			newBar.bar.color = Color.cyan;//colors [i % colors.Length];
+			 
+			//setting the label
 			if (labels.Length <= i) { //check whether enough labels are available for each bar
 				newBar.label.text = "UNDEFINED";
 			} else {
 				newBar.label.text = labels[i];
 			}
-//			newBar.barValue.text = ;
+
+			//set value label
+			newBar.barValue.text = vals[i].ToString();
+			if (rt.sizeDelta.y < 30f) {
+				newBar.barValue.GetComponent<RectTransform>().pivot = new Vector2 (0.5f, 0f);
+				newBar.barValue.GetComponent<RectTransform> ().anchoredPosition = Vector2.zero;  
+			}
 		}
 	}
 }
