f14dd3257f0b3ad0882146307735a8041a56321a
Tobias <tobias.joschko@gmx.de>
2018-01-15 01:34:32 +0100

    add start scene. 'Get Temperature' Button jumps to bar graph scene

---
+++ Assets/Scripts/ButtonStart.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Unit
+
+public class ButtonStart : MonoBehaviour {
+
+	public void ChangeScene(){
+		UnityEngine.SceneManagement.SceneManager.LoadScene ("_scene");
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
